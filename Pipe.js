c3396bf27648d4d28c69ec9ec34cd380f9a9d916
sarahwxie <sarahwxie@gmail.com>
2022-08-30 19:02:05 -0700

    Finished the project

--- Pipe.js
+++ Pipe.js
@@ -1,52 +1,46 @@
 class Pipe {
-  constructor(){
+  constructor(isTop, height) {
     this.width = 100;
-    this.pipegap = 200;
-    this.height = floor(random(canvas.height - this.pipegap - 30));
-    this.x = canvas.width
-  }
-
-  show(){
-    fill(0, 204, 0);
-
-    image(bottomPipeSprite, this.x, canvas.height - this.height);
-    image(topPipeSprite, this.x,  canvas.height - this.height - this.pipegap - 800);
-  }
-
-  update(){
-    this.x -=panSpeed;
-  }
-
-  offscreen(){
-    if (this.x < -canvas.width) {
-      return true;
+    this.height = height;
+    this.x = canvas.width;
+    this.isTop = isTop;
+    if (isTop) {
+      this.topY = 0;
+      this.bottomY = this.height;
     } else {
-      return false;
+      this.topY = canvas.height - this.height;
+      this.bottomY = canvas.height;
     }
   }
 
-  hits(player){
-    // check to see if its hit a pipe
-    var hitTop = (player.y - 20) < (canvas.height - this.height - this.pipegap);
-    var hitBottom = (player.y + 20) > (canvas.height - this.height);
+  show() {
+    // fill(0, 204, 0);
+    // rect(this.x, this.topY, this.width, this.height);
 
-    // check if it's in the pipe's x value
-    var correctX =  (player.x > this.x && player.x < this.x + this.width);
-    // note: notice how it isn't this.x < player.x < this.x + this.width
-    // this is because you need to use the && (the operations don't work like that)
-
-    if ((hitTop && correctX) || (hitBottom && correctX)){
-      return true;
+    if (this.isTop) {
+      image(topPipeSprite, this.x, this.topY + this.height - 800);
     } else {
-      return false;
+      image(bottomPipeSprite, this.x, this.topY);
     }
+
   }
 
-  passedByPlayer(player){
-      // if right side of the pipe is at the players x value, add 1 to the score
-      if (this.x + this.width == player.x) {
-        player.score += 1;
+  update() {
+    this.x -= panSpeed;
+  }
+
+  colided(p) {
+
+    if (p.x + p.size / 2 >= this.x && p.x - p.size / 2 <= this.x + this.width) {
+      if (!this.isTop && p.y + p.size / 2 >= this.topY) {
+        return true;
       }
+      if (this.isTop && p.y - p.size / 2 <= this.bottomY) {
+        return true;
+      }
+
+    }
+    return false;
   }
 
 }
--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 This is a remake of the code from the codebullet video that makes an AI that can play flappy bird perfectly.
 
 The actual flappy bird clone is different from how he did it -- his video is pretty
-fast and a little impossible to follow along in that aspect
+fast and a little impossible to follow along in that aspect, but I revert it back to his method and basically copy his code from there.
 
-I am following his process and making lots of comments along the way to try and learn about javascript, node applications, and AI.
+This is like 0% original code, I just wanted to learn about NEAT ai and node javascript programs.
